69694248c74aa19cebf503ada27d66eb98eb3fd0
Manu Khanna <2manukhanna@gmail.com>
2025-12-04 12:46:54 -0500

    updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Maya-Creamery/",   // <-- IMPORTANT: repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: "dist",          // <-- Optional but recommended
+  }
 });
